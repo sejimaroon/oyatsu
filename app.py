da25cba1599db7c4ee7b5e96bd261426174238ef
sejimaroon <39391127z@gmail.com>
2024-06-02 20:53:31 +0900

    plus

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ image_texts = {
     'image1.jpg': 'アイスです',
     'image2.jpg': '果物です',
     'image3.jpg': 'デブ！',
-    'image4.jpg': 'デブ！！'
+    'image4.jpg': 'デブ！！',
+    'image5.jpg': 'チャーシュー',
+    'image6.jpg': 'トー横女子',
+    'image7.jpg': 'チョコ',
+    'image8.jpg': 'たまには過去の幸せな思い出に浸ってお酒を飲むのもいいでしょうね',
+    'image9.png': '芋的な何か',
+
 }
 
 @app.route('/')
